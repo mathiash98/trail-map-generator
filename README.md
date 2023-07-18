0f3b538785e92fcad3c9607d65e938c633b63fc2
Mathias Haugsbø <mathiash98@gmail.com>
2023-07-19 01:35:39 +0200

    Feat: Download data for kommune from Overpass-turbo

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # Trail map generator
 
-Generate trail map like https://mathiash98.github.io/posts/hjemmelaget-turkart/ automatically by querying data from OpenStreetMap using Overpass Turbo and style as desired in OpenLayers.
+Generate trail map like https://mathiash98.github.io/posts/hjemmelaget-turkart/ automatically by querying data from OpenStreetMap using Overpass Turbo and style as desired in MapboxGL.
 
-Work in progress
+Visit the [demo](https://mathiash98.github.io/trail-map-generator/) to try it out. Input your kommune and click download. Observe that data is downloaded from Overpass-Turbo and rendered in the map.
+
+Download the map as PNG by clicking the print button in the top right corner.
 
 ## Features:
 
 - [x] Render map with OpenLayers
 - [x] Style paths, labels, roads etc
-- [ ] Hillshades
-- [ ] Contour lines (elevation lines)
-- [ ] Icons for peaks
-- [ ] Generate PNG button
-- [ ] Query data from Overpass Turbo
+- [x] Hillshades
+- [x] Contour lines (elevation lines)
+- [x] Icons for peaks
+- [x] Generate PNG button
+- [x] Query data from Overpass Turbo
--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <div id="app">
+    <form
+      style="height: 50px; display: inline-flex"
+      @submit="handleKommuneFormSubmit"
+    >
+      <label for="kommune">Kommune:</label>
+      <input
+        type="text"
+        name="kommune"
+        id="kommune"
+        placeholder="Skriv inn kommune/lokasjon etc"
+        v-model="placeName"
+        :disabled="loadingData"
+      />
+      <input type="submit" value="Download" :disabled="loadingData" />
+      <p v-if="loadingData">Loading...</p>
+      <p>{{ consoleMessages.at(-1) }}</p>
+    </form>
     <div class="main">
       <div id="map-preview"></div>
-      <!-- <div id="style-code">
-        <textarea name="style" id="" cols="30" rows="10"></textarea>
-      </div> -->
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import mapboxgl from "mapbox-gl";
 import type { Map as MapboxMap } from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
@@ -22,13 +36,14 @@ import {
   DPI,
 } from "@watergis/mapbox-gl-export";
 import "@watergis/mapbox-gl-export/css/styles.css";
+import osmtogeojson from "osmtogeojson";
 // [natural=peak]
 import EidfjordPeaks from "./geojson-data/Eidfjord-peaks.geojson";
 // [natural=hill]
 import EidfjordHills from "./geojson-data/Eidfjord-hills.geojson";
-// ["highway"="path"]
+// [highway=path]
 import EidfjordPaths from "./geojson-data/Eidfjord-paths.geojson";
-// ["highway"="piste"]
+// [piste:type=*]
 import EidfjordPiste from "./geojson-data/Eidfjord-piste.geojson";
 // [place=farm]
 import EidfjordFarms from "./geojson-data/Eidfjord-farms.geojson";
@@ -41,37 +56,10 @@ import EidfjordHamlets from "./geojson-data/Eidfjord-hamlets.geojson";
 // [waterway]
 // import EidfjordWaterway from "./geojson-data/Eidfjord-waterway.geojson";
 
-const layers: mapboxgl.AnyLayer[] = [
-  // {
-  //   id: "water",
-  //   source: EidfjordWater,
-  //   type: "fill",
-  //   paint: {
-  //     "fill-color": "#cee0e4",
-  //     "fill-outline-color": "#000",
-  //     "fill-antialias": false,
-  //   },
-  // },
-  // {
-  //   id: "waterway",
-  //   source: EidfjordWaterway,
-  //   type: "line",
-  //   paint: {
-  //     "line-color": "#cee0e4",
-  //     "line-width": 1,
-  //   },
-  // },
-  // {
-  //   id: "highway",
-  //   source: EidfjordHighway,
-  //   type: "line",
-  //   paint: {
-  //     "line-color": "#aaa",
-  //     "line-width": 1.5,
-  //   },
-  // },
+const layers: (mapboxgl.AnyLayer & { query: string; place: string })[] = [
   {
     id: "path",
+    place: "Eidfjord",
     source: EidfjordPaths,
     type: "line",
     paint: {
@@ -79,58 +67,71 @@ const layers: mapboxgl.AnyLayer[] = [
       "line-color": "#ff5c5c",
       "line-width": 1.5,
     },
+    query: "[highway=path]",
   },
   {
     id: "piste",
+    place: "Eidfjord",
     source: EidfjordPiste,
     type: "line",
     paint: {
       "line-color": "#046c8b",
       "line-width": 1.5,
     },
+    query: '["piste:type"]',
   },
   {
     id: "hill",
+    place: "Eidfjord",
     source: EidfjordHills,
     type: "symbol",
     layout: {
       "icon-image": "mountain",
       "text-size": 14,
     },
+    query: "[natural=hill]",
   },
   {
     id: "farm",
+    place: "Eidfjord",
     source: EidfjordFarms,
     type: "symbol",
     layout: {
       "icon-image": "marker",
       "text-size": 18,
     },
+    query: "[place=farm]",
   },
   {
     id: "hamlet",
+    place: "Eidfjord",
     source: EidfjordHamlets,
     type: "symbol",
     layout: {
       "icon-image": "marker",
       "text-size": 18,
     },
+    query: "[place=hamlet]",
   },
   {
     id: "peak",
+    place: "Eidfjord",
     source: EidfjordPeaks,
     type: "symbol",
     layout: {
       "icon-image": "mountain",
       "text-size": 20,
     },
+    query: "[natural=peak]",
   },
 ];
+
+let map: MapboxMap;
 
 onMounted(() => {
   mapboxgl.accessToken =
     "pk.eyJ1IjoibWF0aGlhc2g5OCIsImEiOiJja3c1ZGx6bmcwZmQyMm5sajJrZGQwdDF5In0.Vw5JcsEGSmSzYTVGzhHPNQ";
-  const map = new mapboxgl.Map({
+  map = new mapboxgl.Map({
     container: "map-preview",
     zoom: 10,
     center: [7.1907702, 60.3464172],
@@ -140,12 +141,12 @@ onMounted(() => {
 
   map.addControl(new mapboxgl.NavigationControl());
   map.addControl(new mapboxgl.ScaleControl({}));
+  map.addControl(new mapboxgl.FullscreenControl());
 
-  // create control with specified options
   map.addControl(
     new MapboxExportControl({
       PageSize: Size.A2,
-      PageOrientation: PageOrientation.Portrait,
+      PageOrientation: PageOrientation.Landscape,
       Format: Format.PNG,
       DPI: DPI[400],
       Crosshair: true,
@@ -169,56 +170,114 @@ onMounted(() => {
   (window as any).maplibreMap = map;
 });
 
-function addLayersToMap(map: MapboxMap, layersArr: mapboxgl.AnyLayer[]) {
+function addLayersToMap(
+  map: MapboxMap,
+  layersArr: (mapboxgl.AnyLayer & { query: string; place: string })[]
+) {
   for (const layer of layersArr) {
-    if ("source" in layer) {
-      map.addSource(layer.id, {
-        type: "geojson",
-        data: layer.source,
+    addLayerToMap(map, layer, `${layer.place}-${layer.id}`);
+  }
+}
+
+function addLayerToMap(
+  map: MapboxMap,
+  layer: mapboxgl.AnyLayer,
+  layerId?: string,
+  geoJson?: any
+) {
+  if ("source" in layer) {
+    map.addSource(layerId ?? layer.id, {
+      type: "geojson",
+      data: geoJson ?? layer.source,
+    });
+
+    if (layer.type === "symbol") {
+      map.addLayer({
+        id: layerId ?? layer.id,
+        type: layer.type,
+        source: layerId ?? layer.id,
+        layout: {
+          ...layer.layout,
+          "text-field": [
+            "case",
+            ["has", "ele"],
+            [
+              "to-string",
+              ["concat", ["get", "name"], "\n", ["get", "ele"], " m"],
+            ],
+            ["get", "name"],
+          ],
+          "text-font": ["Spectral Medium Italic"],
+          // "symbol-z-order": "source",
+          "symbol-sort-key": [
+            "case",
+            ["has", "ele"],
+            ["-", 5000, ["to-number", ["get", "ele"]]],
+            5000,
+          ],
+          "text-anchor": "bottom",
+          "text-offset": [0, -0.3],
+        },
+        paint: {
+          "text-color": "#333",
+          "text-halo-color": "#f2f2f2",
+          "text-halo-width": 3,
+        },
+      });
+    } else {
+      map.addLayer({
+        id: layerId ?? layer.id,
+        type: layer.type,
+        source: layerId ?? layer.id,
+        paint: layer.paint as any,
+      });
+    }
+  }
+}
+
+const placeName = ref("Eidfjord");
+const loadingData = ref(false);
+const consoleMessages = ref<string[]>([]);
+async function handleKommuneFormSubmit(e: Event) {
+  e.preventDefault();
+  loadingData.value = true;
+
+  try {
+    const overpassTurboApiUrl = `http://overpass-api.de/api/interpreter`;
+    for (const layer of layers) {
+      const layerId = `${placeName.value}-${layer.id}`;
+      const existingLayerSource = map.getSource(layerId);
+      if (existingLayerSource) {
+        console.info(`${layerId} already exists, skipping...`);
+        consoleMessages.value.push(`${layerId} already exists, skipping...`);
+        continue;
+      }
+
+      console.time(`Fetching ${layerId} data`);
+      const response = await fetch(overpassTurboApiUrl, {
+        method: "POST",
+        body: `[out:json];
+  rel["name"="${placeName.value}"];
+  map_to_area;
+  nwr${layer.query}(area);
+  (._;>;);
+  out body qt;`,
       });
 
-      if (layer.type === "symbol") {
-        map.addLayer({
-          id: layer.id,
-          type: layer.type,
-          source: layer.id,
-          layout: {
-            ...layer.layout,
-            "text-field": [
-              "case",
-              ["has", "ele"],
-              [
-                "to-string",
-                ["concat", ["get", "name"], "\n", ["get", "ele"], " m"],
-              ],
-              ["get", "name"],
-            ],
-            "text-font": ["Spectral Medium Italic"],
-            // "symbol-z-order": "source",
-            "symbol-sort-key": [
-              "case",
-              ["has", "ele"],
-              ["-", 5000, ["to-number", ["get", "ele"]]],
-              5000,
-            ],
-            "text-anchor": "bottom",
-            "text-offset": [0, -0.3],
-          },
-          paint: {
-            "text-color": "#333",
-            "text-halo-color": "#f2f2f2",
-            "text-halo-width": 3,
-          },
-        });
-      } else {
-        map.addLayer({
-          id: layer.id,
-          type: layer.type,
-          source: layer.id,
-          paint: layer.paint as any,
-        });
-      }
+      const data = await response.json();
+      const geojson = osmtogeojson(data);
+      console.info(`${layerId} num features ${geojson.features.length}`);
+      consoleMessages.value.push(
+        `${layerId} num features ${geojson.features.length}`
+      );
+
+      addLayerToMap(map, layer, layerId, geojson);
+
+      console.timeEnd(`Fetching ${layerId} data`);
+      await new Promise((resolve) => setTimeout(resolve, 3000));
     }
+  } finally {
+    loadingData.value = false;
   }
 }
 </script>
@@ -240,10 +299,6 @@ function addLayersToMap(map: MapboxMap, layersArr: mapboxgl.AnyLayer[]) {
 #map-preview {
   width: 100%;
   height: 100%;
-}
-#style-code {
-  width: 100px;
-  height: 100px;
 }
 </style>
 
